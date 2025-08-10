5e71c54dbff121306024dc0629065ebc9cc74d62
Dharshan <cdharshans@gmail.com>
2025-08-10 13:49:19 +0800

    Space added for print

--- src/main/java/com/gic/game/console/MinePlayGroundPrinter.java
+++ src/main/java/com/gic/game/console/MinePlayGroundPrinter.java
@@ -11,7 +11,7 @@ public class MinePlayGroundPrinter {
     public static void displayMinefield(MinePlayBord minefield, boolean revealAll) {
         int size = minefield.getSize();
         System.out.println();
-        System.out.print(" ");
+        System.out.print("  ");
         for (int c = 1; c <= size; c++) {
             System.out.print(c + " ");
         }
